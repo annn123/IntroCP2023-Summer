b1762d0852551afc19eea2d444b3d1ca4a3bd990
annn123 <132078483+annn123@users.noreply.github.com>
2023-09-03 02:19:09 +0800

    Create main.cpp

---
+++ src/hw1-2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[1000][1000];
+    int h,w,b;
+    int halfh,halfw;
+    int ta=0,tb=0,tc=0,td=0;
+    cin>>w>>h;
+    for(int i=0;i<h;i++){
+        for(int j=0;j<w;j++){
+            cin>>a[i][j];
+        }
+    }
+    
+    halfh=h/2;
+    halfw=w/2;
+    
+    
+    for(int i=0;i<halfh;i++){
+        for(int j=0;j<halfw;j++){
+            ta=ta+a[i][j];
+        }
+    }
+    for(int i=0;i<halfh;i++){
+        for(int j=halfw;j<w;j++){
+            tb=tb+a[i][j];
+        }
+    }
+    for(int i=halfh;i<h;i++){
+        for(int j=0;j<halfw;j++){
+            tc=tc+a[i][j];
+        }
+    }
+    for(int i=halfh;i<h;i++){
+        for(int j=halfw;j<w;j++){
+            td=td+a[i][j];
+        }
+    }
+    
+    b=halfh*halfw;
+    cout<<h<<endl;
+    cout<<w<<endl;
+    cout<<b<<endl;
+    cout<<halfh<<endl;
+    cout<<halfw<<endl;
+    cout<<ta/b<<" "<<tb/b<<endl;
+    cout<<tc/b<<" "<<td/b<<endl;
+    
+}
